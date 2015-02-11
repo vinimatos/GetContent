acc4e0c97fbd1446a5fb8a180e729373c5d9fbf2
Vinícius Matos <vinicius@riachao.com>
2015-02-10 23:36:03 -0300

    Inicio da biblioteca de obter contéudo por rss

---
+++ GetUrls/ClassLibrary2/Dominio/TypeUrl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace UrlNames.Dominio
+{
+    public class TypeUrl
+    {
+        public string UrlName { get; set; }
+
+        public TypeUrl(string urlName)
+        {
+            UrlName = urlName;
+        }
+
+        public void GetContentByXML()
+        {
+            var request = WebRequest.Create("http://g1.globo.com/dynamo/rss2.xml");
+
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            var reader = new StreamReader(dataStream);
+            
+
+
+
+        }
+
+    }
+}
